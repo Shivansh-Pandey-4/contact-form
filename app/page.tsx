48c36af08ce0768163849c4127b37aaf66e35470
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2026-01-09 06:49:27 +0530

    feat: create contact us form using shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import FormClient from "@/components/FormClient";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page.</h1>
+    <div className="min-h-screen bg-linear-to-b from-pink-100 to-pink-300 flex flex-col items-center">
+
+      <header className="mt-15 mb-5 text-center">
+        <h1 className="text-4xl font-bold">Server actions Demo</h1>
+
+        <p className="text-xl mt-3 text-gray-600">Contact form with Postgresql and revalidation</p>
+      </header>
+
+      <section>
+        <FormClient />
+      </section>
     </div>
   );
 }
---
+++ components/FormClient.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Card, CardHeader, CardContent, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+
+export default function FormClient() {
+    return (
+        <div>
+            <Card className="w-xl max-w-2xl">
+                <CardHeader>
+                    <CardTitle>
+                        <h1>Contact Us Form</h1>
+                    </CardTitle>
+                </CardHeader>
+
+                <CardContent>
+                    <form action="">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex gap-3">
+                                <div className="flex flex-col gap-1.5 w-full">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input required id="name" name="name" type="text" placeholder="ex: Max William" />
+                                </div>
+                                <div className="flex flex-col gap-1.5 w-full">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input required id="email" name="email" type="email" placeholder="ex: max@gmail.com" />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col gap-1.5">
+                                <Label htmlFor="subject">Subject</Label>
+                                <Input required id="subject" name="subject" type="text" placeholder="enter your subject" />
+                            </div>
+
+                            <div className="flex flex-col gap-1.5">
+                                <Label htmlFor="message">Message</Label>
+                                <Textarea id="message" name="message" placeholder="enter your message? 🤔" />
+                            </div>
+                            <Button className="cursor-pointer">Send Message</Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
